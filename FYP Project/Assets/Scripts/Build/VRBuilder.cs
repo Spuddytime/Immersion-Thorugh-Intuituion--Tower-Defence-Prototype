34a5567d1206e4e5d2039e1af57b34e3e2555a94
Stephen <93441585+Spuddytime@users.noreply.github.com>
2026-03-26 19:27:48 +0000

    Creating a new LineRendered for a raycast

--- FYP Project/Assets/Scripts/Build/VRBuilder.cs
+++ FYP Project/Assets/Scripts/Build/VRBuilder.cs
@@ -1,9 +1,9 @@
 using UnityEngine;
 
-// Oh Ma LAWD he coming
+// Simulates VR-style building using a forward ray and visible laser
 public class VRBuilder : MonoBehaviour
 {
-    public Transform rayOrigin; // camera or controller later
+    public Transform rayOrigin;
     public float rayDistance = 50f;
 
     public LayerMask groundLayer;
@@ -12,47 +12,117 @@ public class VRBuilder : MonoBehaviour
     public BuildableOption[] buildOptions;
     int currentBuildIndex = 0;
 
+    [Header("Laser Visuals")]
+    public LineRenderer laserLine;
+    public Transform hitMarker;
+
+    void Start()
+    {
+        UpdateBuildModeUI();
+    }
+
     void Update()
     {
         HandleInput();
-        UpdateRay();
+        UpdateRayVisuals();
     }
 
     void HandleInput()
     {
-        // simulate trigger with mouse click
         if (Input.GetMouseButtonDown(0))
         {
             TryPlace();
         }
 
-        // switch build types
-        if (Input.GetKeyDown(KeyCode.Alpha1)) currentBuildIndex = 0;
-        if (Input.GetKeyDown(KeyCode.Alpha2)) currentBuildIndex = 1;
-        if (Input.GetKeyDown(KeyCode.Alpha3)) currentBuildIndex = 2;
-        if (Input.GetKeyDown(KeyCode.Alpha4)) currentBuildIndex = 3;
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+            SetBuildMode(0);
+
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+            SetBuildMode(1);
+
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+            SetBuildMode(2);
+
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+            SetBuildMode(3);
     }
 
-    void UpdateRay()
+    void SetBuildMode(int index)
     {
+        if (buildOptions == null || index >= buildOptions.Length)
+            return;
+
+        currentBuildIndex = index;
+        Debug.Log("VR Build Mode: " + buildOptions[index].name);
+        UpdateBuildModeUI();
+    }
+
+    void UpdateBuildModeUI()
+    {
+        if (UIManager.Instance == null || buildOptions == null || buildOptions.Length == 0)
+            return;
+
+        UIManager.Instance.UpdateBuildMode(buildOptions[currentBuildIndex].name);
+    }
+
+    void UpdateRayVisuals()
+    {
+        if (rayOrigin == null)
+            return;
+
         Ray ray = new Ray(rayOrigin.position, rayOrigin.forward);
+
+        Vector3 rayEnd = rayOrigin.position + rayOrigin.forward * rayDistance;
 
         if (Physics.Raycast(ray, out RaycastHit hit, rayDistance, groundLayer))
         {
-            if (GridManager.Instance.GetXY(hit.point, out int x, out int y))
+            rayEnd = hit.point;
+
+            if (hitMarker != null)
             {
-                Vector3 pos = GridManager.Instance.GetWorldPosition(x, y);
-                cellHighlight.position = pos + Vector3.up * 0.05f;
-                cellHighlight.gameObject.SetActive(true);
-                return;
+                hitMarker.position = hit.point;
+                hitMarker.gameObject.SetActive(true);
+            }
+
+            if (GridManager.Instance != null && cellHighlight != null)
+            {
+                if (GridManager.Instance.GetXY(hit.point, out int x, out int y))
+                {
+                    Vector3 cellWorldPos = GridManager.Instance.GetWorldPosition(x, y);
+                    cellHighlight.position = cellWorldPos + new Vector3(0f, 0.05f, 0f);
+                    cellHighlight.gameObject.SetActive(true);
+                }
+                else
+                {
+                    cellHighlight.gameObject.SetActive(false);
+                }
             }
         }
+        else
+        {
+            if (hitMarker != null)
+                hitMarker.gameObject.SetActive(false);
 
-        cellHighlight.gameObject.SetActive(false);
+            if (cellHighlight != null)
+                cellHighlight.gameObject.SetActive(false);
+        }
+
+        if (laserLine != null)
+        {
+            laserLine.positionCount = 2;
+            laserLine.SetPosition(0, rayOrigin.position);
+            laserLine.SetPosition(1, rayEnd);
+        }
     }
 
     void TryPlace()
     {
+        if (rayOrigin == null || GridManager.Instance == null)
+            return;
+
+        if (buildOptions == null || buildOptions.Length == 0)
+            return;
+
         Ray ray = new Ray(rayOrigin.position, rayOrigin.forward);
 
         if (Physics.Raycast(ray, out RaycastHit hit, rayDistance, groundLayer))
@@ -61,21 +131,124 @@ public class VRBuilder : MonoBehaviour
             {
                 BuildableOption option = buildOptions[currentBuildIndex];
 
+                if (EconomyManager.Instance != null && !EconomyManager.Instance.CanAfford(option.cost))
+                {
+                    Debug.Log("Not enough money for " + option.name);
+                    return;
+                }
+
+                bool placedSuccessfully = false;
+
                 switch (option.type)
                 {
                     case BuildType.Wall:
-                        GridManager.Instance.PlaceWall(x, y, option.prefab, option.cost);
+                        placedSuccessfully = TryPlaceWall(x, y, option.prefab, option.cost);
                         break;
 
                     case BuildType.Turret:
-                        GridManager.Instance.PlaceTurret(x, y, option.prefab, option.cost);
+                        placedSuccessfully = TryPlaceTurret(x, y, option.prefab, option.cost);
                         break;
 
                     case BuildType.Trap:
-                        GridManager.Instance.PlaceTrap(x, y, option.prefab, option.cost);
+                        placedSuccessfully = TryPlaceTrap(x, y, option.prefab, option.cost);
                         break;
+                }
+
+                if (placedSuccessfully && EconomyManager.Instance != null)
+                {
+                    EconomyManager.Instance.SpendMoney(option.cost);
                 }
             }
         }
     }
+
+    bool TryPlaceWall(int x, int y, GameObject prefab, int cost)
+    {
+        if (prefab == null)
+            return false;
+
+        if (IsSpecialCell(x, y))
+            return false;
+
+        if (GridManager.Instance.HasWall(x, y))
+            return false;
+
+        if (WouldBlockPath(x, y))
+            return false;
+
+        bool placed = GridManager.Instance.PlaceWall(x, y, prefab, cost);
+
+        if (placed && pathTester != null)
+        {
+            pathTester.TestPath();
+        }
+
+        return placed;
+    }
+
+    bool TryPlaceTurret(int x, int y, GameObject prefab, int cost)
+    {
+        if (prefab == null)
+            return false;
+
+        if (!GridManager.Instance.HasWall(x, y))
+            return false;
+
+        if (GridManager.Instance.HasTurret(x, y))
+            return false;
+
+        return GridManager.Instance.PlaceTurret(x, y, prefab, cost);
+    }
+
+    bool TryPlaceTrap(int x, int y, GameObject prefab, int cost)
+    {
+        if (prefab == null)
+            return false;
+
+        if (GridManager.Instance.HasWall(x, y))
+            return false;
+
+        if (GridManager.Instance.HasTrap(x, y))
+            return false;
+
+        if (IsSpecialCell(x, y))
+            return false;
+
+        return GridManager.Instance.PlaceTrap(x, y, prefab, cost);
+    }
+
+    bool IsSpecialCell(int x, int y)
+    {
+        return false;
+    }
+
+    bool WouldBlockPath(int x, int y)
+    {
+        if (GridManager.Instance == null || Pathfinder.Instance == null)
+            return true;
+
+        int startX, startY, goalX, goalY;
+
+        GameObject startMarker = GameObject.Find("StartMarker");
+        GameObject endMarker = GameObject.Find("EndMarker");
+
+        if (startMarker == null || endMarker == null)
+            return true;
+
+        if (!GridManager.Instance.GetXY(startMarker.transform.position, out startX, out startY))
+            return true;
+
+        if (!GridManager.Instance.GetXY(endMarker.transform.position, out goalX, out goalY))
+            return true;
+
+        GridManager.Instance.SetCellBlocked(x, y, true);
+
+        var testPath = Pathfinder.Instance.FindPath(startX, startY, goalX, goalY);
+
+        GridManager.Instance.SetCellBlocked(x, y, false);
+
+        return testPath == null;
+    }
+
+    public PathTester pathTester;
 }
